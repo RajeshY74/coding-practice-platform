25fc7a07cf8218096a8d8c4daa11723c6f1b2797
Rajesh Yadav <rajesh@rajesh.com>
2026-03-15 19:44:14 +0530

    backend url update

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+const axiosClient = axios.create({
+  baseURL: "https://coding-practice-platform.onrender.com",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 
